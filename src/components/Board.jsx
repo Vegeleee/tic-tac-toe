dfce2665f48327554886882bc7472659d6eb3baa
Vegeleee <danil_vegele@mail.ru>
2020-02-19 01:19:31 +0200

    Square onClick handling

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,7 +3,7 @@ import Square from './Square';
 
 class Board extends React.Component {
 	renderSquare(i) {
-		return <Square />
+		return <Square value={i} />
 	}
 
 	render() {
@@ -14,21 +14,21 @@ class Board extends React.Component {
 				<div className="status">{status}</div>
 
 				<div className="board-row">
+					{this.renderSquare(0)}
 					{this.renderSquare(1)}
 					{this.renderSquare(2)}
-					{this.renderSquare(3)}
 				</div>
 
 				<div className="board-row">
+					{this.renderSquare(3)}
 					{this.renderSquare(4)}
 					{this.renderSquare(5)}
-					{this.renderSquare(6)}
 				</div>
-				
+
 				<div className="board-row">
+					{this.renderSquare(6)}
 					{this.renderSquare(7)}
 					{this.renderSquare(8)}
-					{this.renderSquare(9)}
 				</div>
 			</div>
 		)
