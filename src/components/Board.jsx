e8341ebb3fcaf349b2acb7b017fd8079dd398965
Vegeleee <danil_vegele@mail.ru>
2020-04-04 20:30:34 +0200

    Updated Board.jsx

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,36 +2,50 @@ import React from 'react'
 import Square from './Square'
 
 const Board = ({ squares, onClick }) => {
+	/*
+		Рендерит клетку
+		Принимает порядковый номер клетки
+		Возвращает компонент Square
+	*/
 	const renderSquare = (i) => {
-		return (
-			<Square
-				value={squares[i]}
-				onClick={() => onClick(i)}
-			/>
-		)
+		return <Square value={squares[i]} onClick={() => onClick(i)} />
 	}
 
-	return (
-		<div>
-			<div className="board-row">
-				{renderSquare(0)}
-				{renderSquare(1)}
-				{renderSquare(2)}
-			</div>
+	/*
+		Рендерит строку клеток на игровом поле
+		Принимает номер строки и количество колонок
+		Возвращает массив клеток, длиной colsCount
+	*/
+	const renderRow = (rowNumber, colsCount) => {
+		const row = []
 
-			<div className="board-row">
-				{renderSquare(3)}
-				{renderSquare(4)}
-				{renderSquare(5)}
-			</div>
+		for (
+			let i = rowNumber * colsCount;
+			i < rowNumber * colsCount + colsCount;
+			i++
+		) {
+			row.push(renderSquare(i))
+		}
+		return row
+	}
 
-			<div className="board-row">
-				{renderSquare(6)}
-				{renderSquare(7)}
-				{renderSquare(8)}
-			</div>
-		</div>
-	)
+	/*
+		Рендерит игровое поле
+		Принимает количество строк и количество колонок
+		Возвращает массив эл-ов div, длиной rowsCount
+	*/
+	const renderBoard = (rowsCount, colsCount) => {
+		const board = []
+
+		for (let i = 0; i < rowsCount; i++) {
+			const row = <div className="board-row">{renderRow(i, colsCount)}</div>
+			board.push(row)
+		}
+
+		return board
+	}
+
+	return <React.Fragment>{renderBoard(3, 3)}</React.Fragment>
 }
 
 export default Board
