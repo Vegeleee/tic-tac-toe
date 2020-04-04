342ceca73f4e5d19d8ee82923747a397c51903bb
Vegeleee <danil_vegele@mail.ru>
2020-04-04 18:51:02 +0200

    Updates

---
+++ precache-manifest.f68d6ece4a844aa9f204e6c5f848ddf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06975bdf7e770206aba33dc5eea22268",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "6425627b5cffb249ab72",
+    "url": "/tic-tac-toe/static/css/main.151ee975.chunk.css"
+  },
+  {
+    "revision": "562460db104b4653e763",
+    "url": "/tic-tac-toe/static/js/2.ae655ebc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tic-tac-toe/static/js/2.ae655ebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6425627b5cffb249ab72",
+    "url": "/tic-tac-toe/static/js/main.db94589c.chunk.js"
+  },
+  {
+    "revision": "fdbd6fae367d142c593b",
+    "url": "/tic-tac-toe/static/js/runtime-main.df4931a8.js"
+  }
+]);
